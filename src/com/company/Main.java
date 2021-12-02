9dad107ba1a8306da9be8a218d7a5d1ade040e23
Arman Karapetyan <armanmad93@gmail.com>
2021-12-02 20:30:41 +0400

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getEncodingString("aabbbc"));
+        System.out.println(getEncodingString("ssiiggkooo"));
     }
 
     public static String getEncodingString(String word) {
         StringBuilder stringBuilder = new StringBuilder();
-        int count = 1;
+        int count = 0;
         char[] chars = word.toCharArray();
 
         for (int i = 0; i < chars.length - 1; i++) {
             if (chars[i] != chars[i + 1]) {
-                stringBuilder.append(i + count);
+                stringBuilder.append(i + 1 - count);
                 stringBuilder.append(chars[i]);
-                count = i;
+                count = i + 1;
             }
         }
         return stringBuilder.toString();
